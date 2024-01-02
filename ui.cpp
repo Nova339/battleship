eaee867c1da3738940ed102ace4e98ab79271f94
Nova339 <79296461+Nova339@users.noreply.github.com>
2024-01-01 17:44:12 -0700

    Create ui.cpp

---
+++ ui.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <string>
+using namespace std;
+
+void logo(){
+    cout << "******      ****    ******  ******  **      ********  " << endl;
+    cout << "******      ****    ******  ******  **      ********  " << endl;
+    cout << "**    **  **    **    **      **    **      **        " << endl;
+    cout << "**    **  **    **    **      **    **      **        " << endl;
+    cout << "******    ********    **      **    **      ******    " << endl;
+    cout << "******    ********    **      **    **      ******    " << endl;
+    cout << "**    **  **    **    **      **    **      **        " << endl;
+    cout << "**    **  **    **    **      **    **      **        " << endl;
+    cout << "******    **    **    **      **    ******  ********  " << endl;
+    cout << "******    **    **    **      **    ******  ********  " << endl;
+    cout << endl;
+    cout << endl;
+    cout << "  ******  **    **  **  ******  " << endl;
+    cout << "  ******  **    **  **  ******  " << endl;
+    cout << "**        **    **  **  **    **" << endl;
+    cout << "**        **    **  **  **    **" << endl;
+    cout << "  ****    ********  **  ******  " << endl;
+    cout << "  ****    ********  **  ******  " << endl;
+    cout << "      **  **    **  **  **      " << endl;
+    cout << "      **  **    **  **  **      " << endl;
+    cout << "******    **    **  **  **      " << endl;
+    cout << "******    **    **  **  **      " << endl;
+    cout << endl;
+    cout << endl;
+}
+
+void displayWin(){    
+    cout << "**  **    ****    **    **" << endl;
+    cout << "**  **    ****    **    **" << endl;
+    cout << "**  **  **    **  **    **" << endl;
+    cout << "**  **  **    **  **    **" << endl;
+    cout << "******  **    **  **    **" << endl;
+    cout << "******  **    **  **    **" << endl;
+    cout << "  **    **    **  **    **" << endl;
+    cout << "  **    **    **  **    **" << endl;
+    cout << "  **      ****      ****  " << endl;
+    cout << "  **      ****      ****  " << endl;
+    cout << endl;
+    cout << endl;
+    cout << "**      **  **  **    **" << endl;
+    cout << "**      **  **  **    **" << endl;
+    cout << "**      **  **  ****  **" << endl;
+    cout << "**      **  **  ****  **" << endl;
+    cout << "**      **  **  **  ****" << endl;
+    cout << "**      **  **  **  ****" << endl;
+    cout << "**  **  **  **  **    **" << endl;
+    cout << "**  **  **  **  **    **" << endl;
+    cout << "  **  **    **  **    **" << endl;
+    cout << "  **  **    **  **    **" << endl;
+    cout << endl;
+    cout << endl;
+}
+
+void displayLose(){
+    cout << "**  **    ****    **    **" << endl;
+    cout << "**  **    ****    **    **" << endl;
+    cout << "**  **  **    **  **    **" << endl;
+    cout << "**  **  **    **  **    **" << endl;
+    cout << "******  **    **  **    **" << endl;
+    cout << "******  **    **  **    **" << endl;
+    cout << "  **    **    **  **    **" << endl;
+    cout << "  **    **    **  **    **" << endl;
+    cout << "  **      ****      ****  " << endl;
+    cout << "  **      ****      ****  " << endl;
+    cout << endl;
+    cout << endl;
+    cout << "**        ****      ******  ********" << endl;
+    cout << "**        ****      ******  ********" << endl;
+    cout << "**      **    **  **        **      " << endl;
+    cout << "**      **    **  **        **      " << endl;
+    cout << "**      **    **    ****    ******  " << endl;
+    cout << "**      **    **    ****    ******  " << endl;
+    cout << "**      **    **        **  **      " << endl;
+    cout << "**      **    **        **  **      " << endl;
+    cout << "******    ****    ******    ********" << endl;
+    cout << "******    ****    ******    ********" << endl;
+    cout << endl;
+    cout << endl;
+}
